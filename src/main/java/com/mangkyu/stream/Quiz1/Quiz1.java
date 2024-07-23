caa9ed9d7c1947e2cd6df225d3e647e387dd2621
wooandrich <brad1182@naver.com>
2024-07-23 14:39:24 +0900

    퀴즈1-1 풀이 설명추가

--- src/main/java/com/mangkyu/stream/Quiz1/Quiz1.java
+++ src/main/java/com/mangkyu/stream/Quiz1/Quiz1.java
@@ -18,9 +18,9 @@ public class Quiz1 {
 
 
         return csvLines.stream()
-                .map(line -> line[1].replaceAll("\\s", ""))
-                .flatMap(hobbies -> Arrays.stream(hobbies.split(":")))
-                .collect(Collectors.toMap(hobby -> hobby, hobby -> 1, (oldValue, newValue) -> newValue += oldValue));
+                .map(line -> line[1].replaceAll("\\s", "")) // 띄어쓰기 제거
+                .flatMap(hobbies -> Arrays.stream(hobbies.split(":"))) // : 로 구분후 flatMap변환
+                .collect(Collectors.toMap(hobby -> hobby, hobby -> 1, (oldValue, newValue) -> newValue += oldValue)); // Map형태로 변환, 키 중복시 +1
     }
 
     // 1.2 각 취미를 선호하는 정씨 성을 갖는 인원이 몇 명인지 계산하여라.
